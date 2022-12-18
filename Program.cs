df00579feeab1c20c08512bb75c8e861e6a3e9f8
OVGI <girichevao@gmail.com>
2022-12-18 17:08:45 +0300

    task5 zad33

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
-﻿
-int a = new Random().Next(1000);
-
-Console.Write($"digits in {a}={DigitsCount(a)}");
-
-int DigitsCount(int _a)
+﻿/* Задача 33. Задайте массив. Напишите программу, 
+которая определяет, присутствует ли заданное число в массиве.  */
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
 {
-    int k = 0;
-    while (_a > 0)
-    {
-        _a /= 10;
-        k++;
-    }
-    return k;
+    array[i] = new Random().Next(10);
+    Console.Write($"{array[i]} ");
 }
+int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+int s = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == a)
+    {
+        Console.Write($"Да");
+        break;
+    }
+    s = s + 1;
+}
+if (s == array.Length)
+    Console.Write($"Нет");
