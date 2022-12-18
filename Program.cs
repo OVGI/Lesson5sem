ec78f4d49acca500952c7bcc71aa47f86cfe3d66
OVGI <girichevao@gmail.com>
2022-12-18 16:09:04 +0300

    task3 zad32 void Print

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 32 
+
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-9, 10);
+Print(array);
+
+Invers(array);
+
+Print(array);
+
+void Invers(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+
+        arr[i] = -arr[i];
+}
+
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
